705bea8abfa3185badd5f9d758b8aa732ecc6218
Abdelrahman arfat <abdoarfat924@gmail.com>
2024-03-13 13:37:54 +0200

    .

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,12 @@
 import 'package:ecommerce_advanced/e_commerce_app.dart';
+import 'package:ecommerce_advanced/firebase_options.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(const MyApp());
 }
-
-
