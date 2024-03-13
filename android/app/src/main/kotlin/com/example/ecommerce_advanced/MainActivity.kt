b8da95aab7ecc9740cf5e649a0108f343fe57037
Abdelrahman arfat <abdoarfat924@gmail.com>
2024-03-13 11:12:12 +0200

    first commit

---
+++ android/app/src/main/kotlin/com/example/ecommerce_advanced/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.ecommerce_advanced
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,8 @@
+import 'package:ecommerce_advanced/e_commerce_app.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+
