73a79fd2836c7c93a830055e908c0b1852c296dc
Abdelrahman arfat <abdoarfat924@gmail.com>
2024-03-14 08:44:45 +0200

    add native splash screen for all plat form and all android version add screen utill configration

--- lib/e_commerce_app.dart
+++ lib/e_commerce_app.dart
@@ -2,6 +2,7 @@ import 'package:ecommerce_advanced/core/app/connectivity_controller.dart';
 import 'package:ecommerce_advanced/core/app/env.varibles.dart';
 import 'package:ecommerce_advanced/core/common/screens/no_network_screen.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
@@ -12,26 +13,31 @@ class MyApp extends StatelessWidget {
       valueListenable: ConnectivityController.instance.isConnected,
       builder: (_, value, __) {
         if (value) {
-          return MaterialApp(
-            title: 'Asroo Store',
-            debugShowCheckedModeBanner: EnvVariable.instance.debugMode,
-            theme: ThemeData(
-              colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-              useMaterial3: true,
-            ),
-            builder: (context, widget) {
-              return Scaffold(
-                body: Builder(
-                  builder: (context) {
-                    ConnectivityController.instance.init();
-                    return widget!;
-                  },
+          return ScreenUtilInit(
+  designSize: const Size(375, 812),
+      minTextAdapt: true,
+      splitScreenMode: true,
+            child: MaterialApp(
+              title: 'Asroo Store',
+              debugShowCheckedModeBanner: EnvVariable.instance.debugMode,
+              theme: ThemeData(
+                colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+                useMaterial3: true,
+              ),
+              builder: (context, widget) {
+                return Scaffold(
+                  body: Builder(
+                    builder: (context) {
+                      ConnectivityController.instance.init();
+                      return widget!;
+                    },
+                  ),
+                );
+              },
+              home: Scaffold(
+                appBar: AppBar(
+                  title: const Text('Asroo Store'),
                 ),
-              );
-            },
-            home: Scaffold(
-              appBar: AppBar(
-                title: const Text('Asroo Store'),
               ),
             ),
           );
